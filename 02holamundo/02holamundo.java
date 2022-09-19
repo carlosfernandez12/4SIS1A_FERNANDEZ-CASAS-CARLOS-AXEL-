c756927e297d462f50fa69f621ecf7c7ffe45d63
carlosfernandez12 <c.fernandez.c069@edu.utc.mx>
2022-09-19 11:05:25 -0500

    hola mundo

---
+++ 02holamundo/02holamundo.java
@@ -0,0 +1,17 @@
+//esto es un comentario
+/*
+* ete comentario nos sirve para poder tener lineas comentadas de codigo 
+*todo programa en java,manejaclases,una clase es un objreto del mundo real
+*/
+class holamundo{
+//cuerpo del programa 
+/*
+*aqui es donde nosotros codificamos todos los elementos del programa,funciones,estructuras,variables y metodos 
+*toda clae debe tener un metodo principal 
+*/
+
+public static void main(String[] args){
+System.out.println("Hola Mundo");
+
+
+}}
