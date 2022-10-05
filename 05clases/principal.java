05a726aa56adbd8560235c7deff74ad3a4e98ea7
carlosfernandez12 <c.fernandez.c069@edu.utc.mx>
2022-10-04 19:19:33 -0500

    nuevo archivo

--- 05clases/principal.java
+++ 05clases/principal.java
@@ -1,10 +1,10 @@
-class Principal {
+class principal {
 
     //metodo principal
     public static void main(String[] args){
         //vamos a crear una instancia
-        Ejercicios ejer = new Ejercicios();
+        ejercicios ejer = new ejercicios();
         ejer.menu();
     }
-
+    
 }
