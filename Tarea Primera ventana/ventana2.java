3e581ea467a4addaaf98db907f910637502e6b7b
carlosfernandez12 <c.fernandez.c069@edu.utc.mx>
2022-11-06 20:13:44 -0600

    tarea primera ventana

--- Tarea Primera ventana/ventana2.java
+++ Tarea Primera ventana/ventana2.java
@@ -1,127 +1,144 @@
-//Fernandez casas carlos axel 
-//Tare de primera ventana
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import static java.awt.Color.black;
+import static java.awt.Color.blue;
+import static java.awt.Color.white;
 
-public class ventana2 extends JFrame {
 
-    private JPanel mainPanel;
-    private JTextField userBox;
-    private JPasswordField passBox;
-
-    public ventana2() {
-        this.setTitle("Java Ticket System");
-        this.setSize(370, 170);
-        this.setLocationRelativeTo(null);
-        this.setResizable(false);
-        iniciarComponentes();
-
-    }
-
-    //Metodo para inicializar
-    private void iniciarComponentes() {
-        colocarPaneles();
-        colocarBotones();
-        colocarEtiquetas();
-        colocarCajasTexto();
+public class Ventana2 extends JFrame{
+    public  JPanel panel;
+    public Ventana2(){
+    
+        this.setSize(800,500);
+        setTitle("REGISRTO DE MIMIDOS");
+        setLocation(500,250); //localisacion de tu pantalla
+        setLocationRelativeTo(null);//centro de pantalla pra todo tipo de tamaño de compu
+        JLabel etiqueta1 = new JLabel();
+        etiqueta1.setText("REGISRTO DE MIMIDOS");
        
-
+        iniciarComponentes();
+         panel.add(etiqueta1);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+            
+        
     }
 
-    private void colocarPaneles() {
-        //Panel principal
-        mainPanel = new JPanel();
-        mainPanel.setLayout(null);
-        this.getContentPane().add(mainPanel); // Agregamos el panel
+    
+    private void iniciarComponentes(){
 
-    }
-
-    private void colocarBotones() {
-
-        userBox = new JTextField();
-        passBox = new JPasswordField();
-
-        //Boton de salir
-        JButton exitButton = new JButton("Registrarse");
-        exitButton.setBounds(180, 80, 120, 30);
-        exitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-        mainPanel.add(exitButton);
-
-        //Boton de login (en este tengo el problema)
-
-        JButton loginButton = new JButton("iniciar Sesion");
-        loginButton.setBounds(10, 80, 120, 25);
-        mainPanel.add(loginButton);
-
-        ActionListener userLoginButton = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                String adminUser = "Axel";
-                String adminPass = "Fernandez";
-
-                if (adminUser.equals(userBox.getText())) {
-                    String contrasena = "";
-
-                    for (int i = 0; i < passBox.getPassword().length; i++) {
-                        contrasena += passBox.getPassword()[i];
-                    }
-
-                    if (adminPass.equals(contrasena)) {
-                        JOptionPane.showMessageDialog(null, "Bienvenido");
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Error, contraseña incorrecta");
-                    }
-
-                } else {
-                    JOptionPane.showMessageDialog(null, "Usuario inexistente");
-                }
-
-
-            }
-        };
-
-        loginButton.addActionListener(userLoginButton);
-    }
-
-    private void colocarEtiquetas() {
-
+        colocarPaneles();
+        colocarEtiquetas();
+        colocarBotones();
         
 
-
-        //Etiqueta de usuario y contraseña
-        JLabel userLabel = new JLabel("Usuario");
-        userLabel.setBounds(10, 10, 120, 50);
-        mainPanel.add(userLabel);
-
-        JLabel userPass = new JLabel("Contraseña");
-        userPass.setBounds(10, 40, 80, 50);
-        mainPanel.add(userPass);
-
+    
+    }
+    
+    private void colocarPaneles(){
+        
+     panel = new JPanel();
+    
+    panel.setBackground(black);
+    panel.setLayout(null);
+    this.getContentPane().add(panel);
+        
+    
+  
+    }
+    
+    private void colocarEtiquetas(){
+          JLabel etiqueta = new JLabel();
+    etiqueta.setText("NOMBRE:");
+    etiqueta.setBounds(10,30,60,20);
+    etiqueta.setOpaque(true);
+    etiqueta.setForeground(white);
+    etiqueta.setBackground(blue);
+    
+            //campo de texto
+        JTextField usertext = new JTextField(20);
+        usertext.setBounds(80, 30, 130, 20);
+        panel.add(usertext);
+    panel.add(etiqueta);
+            
+    JLabel etiqueta2 = new JLabel();
+    etiqueta2.setText("APELLIDO PATERNO:");
+    etiqueta2.setBounds(10,60,120,20);
+    etiqueta2.setOpaque(true);
+    etiqueta2.setForeground(white);
+    etiqueta2.setBackground(blue);
+    
+         //campo de texto
+        JTextField text = new JTextField(20);
+        text.setBounds(140, 60, 130, 20);
+        panel.add(text);
+    panel.add(etiqueta2);
+    
+       JLabel etiqueta3 = new JLabel();
+    etiqueta3.setText("APELLIDO MATERNO:");
+    etiqueta3.setBounds(10,90,120,20);
+    etiqueta3.setOpaque(true);
+    etiqueta3.setForeground(white);
+    etiqueta3.setBackground(blue);
+    
+      //campo de texto
+        JTextField texto = new JTextField(20);
+        texto.setBounds(140, 90, 130, 20);
+        panel.add(texto);
+    
+    panel.add(etiqueta3);
+    
+       JLabel etiqueta4 = new JLabel();
+    etiqueta4.setText("FECHA DE NACIMIENTO:");
+    etiqueta4.setBounds(10,120,150,20);
+    etiqueta4.setOpaque(true);
+    etiqueta4.setForeground(white);
+    etiqueta4.setBackground(blue);
+    
+      //campo de texto
+        JTextField texto2 = new JTextField(20);
+        texto2.setBounds(170, 120, 130, 20);
+        panel.add(texto2);
+    
+    panel.add(etiqueta4);
+    
+       JLabel etiqueta5 = new JLabel();
+    etiqueta5.setText("USUARIO:");
+    etiqueta5.setBounds(10,150,70,20);
+    etiqueta5.setOpaque(true);
+    etiqueta5.setForeground(white);
+    etiqueta5.setBackground(blue);
+    
+      //campo de texto
+        JTextField texto3 = new JTextField(20);
+        texto3.setBounds(90, 150, 130, 20);
+        panel.add(texto3);
+    
+    panel.add(etiqueta5);
+    
+       JLabel etiqueta6 = new JLabel();
+    etiqueta6.setText("CONTRASEÑA:");
+    etiqueta6.setBounds(10,180,100,20);
+    etiqueta6.setOpaque(true);
+    etiqueta6.setForeground(white);
+    etiqueta6.setBackground(blue);
+    
+      //campo de texto
+        JTextField texto4 = new JTextField(20);
+        texto4.setBounds(120, 180, 130, 20);
+        panel.add(texto4);
+    
+    panel.add(etiqueta6);
+    }
+    
+    private void colocarBotones(){
+        JButton boton1 = new JButton("ENVIAR");
+        boton1.setBounds(450,380,120,30);
+        boton1.setEnabled(true);
+        panel.add(boton1);
+        
+        
     }
 
-    private void colocarCajasTexto() {
-        //Cajas de Texto
-        JTextField userBox = new JTextField();
-        userBox.setBounds(160, 20, 160, 25);
-        mainPanel.add(userBox);
 
-        JTextField passBox = new JPasswordField();
-        passBox.setBounds(160, 50, 160, 25);
-        mainPanel.add(passBox);
-
-    }
-    public static void main(String[] args) {
-        ventana2 mainPanel = new ventana2();
-        mainPanel.setVisible(true);
-
-    }
+    
 }
